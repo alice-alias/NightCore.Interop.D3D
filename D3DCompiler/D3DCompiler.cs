ddef8b384cda2f73ef713446df4f603827ae7e77
alice-alias <alice-alias@arith.jp>
2020-01-27 08:23:21 +0900

    Add shader macro

--- D3DCompiler/D3DCompiler.cs
+++ D3DCompiler/D3DCompiler.cs
@@ -5,33 +5,37 @@ using System.Text;
 
 namespace NightCore.Interop.D3D
 {
-    public static class D3DCompiler
+    public class D3DCompiler
     {
-        public static D3DCompileResult Compile(string source, string name, string entrypoint, D3DCompilerTarget target)
-            => Compile(source, name, entrypoint, target, null);
+        public ID3DInclude Include { get; set; }
 
-        public static D3DCompileResult Compile(string source, string name, string entrypoint, D3DCompilerTarget target, ID3DInclude include)
+        public Dictionary<string, string> Macros { get; } = new Dictionary<string, string>();
+
+        public D3DCompileResult Compile(string source, string name, string entrypoint, D3DCompilerTarget target)
         {
             var src = Encoding.ASCII.GetBytes(source);
             var pSrc = GCHandle.Alloc(src, GCHandleType.Pinned);
-            var includeVtbl = new D3DIncludeVTable(include);
+            var includeVtbl = new D3DIncludeVTable(Include);
             try
             {
-                var hresult = NativeMethods.D3DCompile(
-                    pSrc.AddrOfPinnedObject(),
-                    (IntPtr)src.Length,
-                    name,
-                    IntPtr.Zero,
-                    include != null ? includeVtbl.Pointer : IntPtr.Zero,
-                    entrypoint,
-                    target,
-                    0,
-                    0,
-                    out var code,
-                    out var err);
-                var errBuf = err.GetBytes();
-                var errStr = errBuf != null ? Encoding.ASCII.GetString(errBuf) : null;
-                return new D3DCompileResult(code.GetBytes(), errStr, hresult);
+                using (var macros = new D3DShaderMacroCollection(Macros))
+                {
+                    var hresult = NativeMethods.D3DCompile(
+                        pSrc.AddrOfPinnedObject(),
+                        (IntPtr)src.Length,
+                        name,
+                        macros.Pointer,
+                        Include != null ? includeVtbl.Pointer : IntPtr.Zero,
+                        entrypoint,
+                        target,
+                        0,
+                        0,
+                        out var code,
+                        out var err);
+                    var errBuf = err.GetBytes();
+                    var errStr = errBuf != null ? Encoding.ASCII.GetString(errBuf) : null;
+                    return new D3DCompileResult(code.GetBytes(), errStr, hresult);
+                }
             }
             finally
             {
---
+++ D3DCompiler/D3DShaderMacroCollection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace NightCore.Interop.D3D
+{
+    sealed class D3DShaderMacroCollection : IDisposable
+    {
+        public IntPtr Pointer { get; private set; }
+
+        public D3DShaderMacroCollection(IDictionary<string, string> data)
+        {
+            var stride = Marshal.SizeOf<IntPtr>();
+
+            var pointers = new List<IntPtr>();
+
+            foreach (var (key, value) in data)
+            {
+                pointers.Add(AllocString(key));
+                pointers.Add(AllocString(value));
+            }
+            pointers.Add(IntPtr.Zero);
+            pointers.Add(IntPtr.Zero);
+
+            var pointersArray = pointers.ToArray();
+            Pointer = Marshal.AllocCoTaskMem(stride * pointersArray.Length);
+            Marshal.Copy(pointersArray, 0, Pointer, pointersArray.Length);
+        }
+
+        static IntPtr AllocString(string str)
+        {
+            var bytes = Encoding.ASCII.GetBytes(str);
+            var ptr = Marshal.AllocCoTaskMem(bytes.Length);
+            Marshal.Copy(bytes, 0, ptr, bytes.Length);
+            return ptr;
+        }
+
+        int locker;
+
+        public void Dispose()
+        {
+            if (Interlocked.Exchange(ref locker, 1) == 0)
+            {
+                var current = Pointer;
+                var stride = Marshal.SizeOf<IntPtr>();
+                while (true)
+                {
+                    var ptr = Marshal.ReadIntPtr(current);
+                    if (ptr == IntPtr.Zero)
+                        break;
+                    Marshal.FreeCoTaskMem(ptr);
+                    current = IntPtr.Add(current, stride);
+                }
+                Marshal.FreeCoTaskMem(Pointer);
+                Pointer = IntPtr.Zero;
+            }
+        }
+
+        ~D3DShaderMacroCollection()
+        {
+            Dispose();
+        }
+    }
+}
